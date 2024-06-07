34c26ae34b56a725344bc9728ddb26168c72a2c3
rudfkr <rudfkr248@gmail.com>
2024-06-07 21:29:37 +0900

    animatedText

--- AnimatedText/5641878/script.js
+++ AnimatedText/5641878/script.js
@@ -1,12 +1,12 @@
-const text = "Animmmmmated Text";
-const textWrapper = document.querySelector(".animated-text");
-const textArray = text.split('');
+const text = "Anima~~~~~~ted Text";
+const textWrapper = document.querySelector(".animated-text"); // DOM
+const textArray = text.split(''); // 문자열 분리
 
-textArray.forEach(letter => {
-    const letterElement = document.createElement("span");
+textArray.forEach(letter => { // 각 요소에 대해 반복
+    const letterElement = document.createElement("span"); // 새 span 요소 추가
 
     letterElement.textContent = letter;
     letterElement.classList.add("letter");
 
-    textWrapper.appendChild(letterElement);
+    textWrapper.appendChild(letterElement); // 추가
 })
